31898926c05482b3de211153de34f9847263ee8f
sukruti <doshisukruti4@gmail.com>
2026-01-13 08:24:03 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,50 @@
+# calculate simple interest
+"""principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the annual interest rate (in %): "))
+time = float(input("Enter the time (in years): "))
+
+simple_interest = (principal * rate * time) / 100
+
+print(f"The simple interest is: {simple_interest}")
+"""
+#maximum no.
+"""
+a=input("enter A:--")
+b=input("enter B:--")
+c=input("enter C:--")
+maximum=max(a ,b ,c )
+print("maximum value is:-- ", maximum)
+
+#print 1 to 5
+
+i=1
+while i<=5:
+    print(i)
+    i+=1
+
+#find length of a string
+text=input("enter string=")
+length=len(text)
+print("length of string is:-",length)
+
+#print message
+
+print("well come")
+
+#first character print
+text=input("enter string:-")
+print("the first character is=",text[0])
+
+#last character print
+text=input("enter string:-")
+print("the first character is=",text[-1])
+"""
+# add three numbers
+a=10
+b=20
+c=40
+print(a+b+c)
+
+#take input from user and print
+a1=input("enter string")
+print(a1)
