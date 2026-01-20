1e4d52a278028234b75d5c17ffe761b6793565ec
sukruti <doshisukruti4@gmail.com>
2026-01-20 08:27:13 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,20 @@
+#data type
+"""a=10
+print(type(a))
+b=7.99
+print(type(b))
+c="smile"
+print(type(c))
+d=1j
+print(type(d))
+
+e=["apple","graps","banana"]
+print(type(e))
+
+f=("jay","raj","ram")
+print(type(f))
+h={"sita","geeta","pari"}
+print(type(h))"""
+
+i=frozenset({"butterfly","sky"})
+print(type(i))
