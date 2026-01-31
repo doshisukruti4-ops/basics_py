832585d9cd68318509f7e4fa45d45bb80c432e15
sukruti <doshisukruti4@gmail.com>
2026-01-31 08:53:34 +0530

    Add files via upload

---
+++ O_statement.py
@@ -0,0 +1,23 @@
+#output statements
+
+#1.basic print
+print("hello atmiya!!..")
+
+#2.print variable
+name='abc'
+age=18
+print(name)
+print(age)
+
+#input statements
+
+#1.take  string input from user
+ 
+name=input("enter your name:")
+print("hello",name)
+
+#2.take integer input from user
+
+age=input("enter your age:")
+print(age)
+
