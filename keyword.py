e047fdbe11a50e32c394fe95517721f130d675ed
sukruti <doshisukruti4@gmail.com>
2026-03-17 08:21:55 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,17 @@
+def student_info(name,age,city):
+    print("name:",name)
+    print("Age:",age)
+    print("city:",city)
+student_info(age=18,city='rajkot',name="ravi")  
+
+def display(a,b,c):
+    print("a=",a)
+    print("b=",b)
+    print("c=",c)
+display(1,c=3,b=2)      
+.3
+
+def simple_interest(p:float,r:int,t:float):
+    si=(p*r*t)/100
+    print("simple interest:",si)
+simple_interest(p=10000,t=2,r=1.5)
